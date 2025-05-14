9ed92a5d48437670deccc6a858e7e2ee7aa4ca82
KameiMzk <mizuki.2004.0422@au.com>
2025-05-14 14:10:55 +0900

    testMove

--- flaskr/myproject/app.py
+++ flaskr/myproject/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
 @app.route('/')
-def index():
+def title():
+    return render_template('title.html')
+@app.route('/select')
+def select():
     return render_template('index.html')
 
 if __name__ == '__main__':
---
+++ flaskr/myproject/templates/move.js
@@ -0,0 +1,6 @@
+document.addEventListener("click", navigateToSelect);
+document.addEventListener("touchstart", navigateToSelect);
+
+function navigateToSelect() {
+    window.location.href = "/select";
+}
